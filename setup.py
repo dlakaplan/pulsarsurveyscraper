25ea10a4d4167dfc3bc8caf1a5509101dcbbf7b8
David Kaplan <kaplan@uwm.edu>
2020-12-10 19:08:14 -0600

    made a package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pulsarsurveyscraper",
+    version="0.1.0",
+    description="Cache pulsar surveys and catalogs; allow API-like access",
+    author="David Kaplan",
+    author_email="kaplan@uwm.edu",
+    url="",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "cache_survey=pulsarsurveyscraper.scripts.cache_survey:main",
+        ],
+    },
+    python_requires=">=3.7",
+    include_package_data=True,
+    zip_safe=False,
+)
